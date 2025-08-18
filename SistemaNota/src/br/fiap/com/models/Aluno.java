576ac4a7f51401aa7ffc2c416733c2a970d63418
manu <haru.noom0@gmail.com>
2025-08-18 19:54:53 -0300

    começando atividade sobre gson

---
+++ SistemaNota/src/br/fiap/com/models/Aluno.java
@@ -0,0 +1,45 @@
+package br.fiap.com.models;
+
+public class Aluno {
+    private String nome;
+    private int idade;
+    private double nota1;
+    private double nota2;
+
+    public Aluno (String nome, int idade, double nota1, double nota2 ) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+
+    }
+
+    //getter
+    public String getNome() {return nome;}
+    public int getIdade() {return idade;}
+    public double getNota1() {return nota1;}
+    public double getNota2() {return nota2;}
+
+    //setter
+    public void setNome(String nome) {this.nome = nome;}
+    public void setIdade(int idade) {this.idade = idade;}
+    public void setNota1(double nota1) {this.nota1 = nota1;}
+    public void setNota2(double nota2) {this.nota2 = nota2;}
+
+    public double calcularMedia(){
+        return (this.nota1 + this.nota2) / 2;
+
+    }
+
+    @Override
+    public String toString(){
+        return "------------------------------\n" +
+                "Nome: " + getNome() + "\n" +
+                "idade" + getIdade() + "\n" +
+                "Media: " +  calcularMedia();
+    }
+
+
+
+}
+
---
+++ SistemaNota/src/br/fiap/com/teste/TesteNota.java
@@ -0,0 +1,16 @@
+package br.fiap.com.teste;
+
+import br.fiap.com.models.Aluno;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TesteNota {
+    private static final String ARQUIVO = "dados.txt";
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List <Aluno> alunos = new ArrayList<>();
+
+    }
+}
